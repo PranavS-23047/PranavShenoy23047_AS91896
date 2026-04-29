3654af1ffd1e6e066ff1b8c9b1d35fcc25154560
PranavS-23047 <23047@students.mrgs.school.nz>
2026-04-29 14:44:24 +1200

    Making Changes

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
+# start by importing tkinter
 import tkinter as tk
 root = tk.Tk()
+
+# setting the title of the program
 root.title("Guess The Number")
+
+
 label = tk.Label(root, text="Hello")
 label.pack()
 root.mainloop()
