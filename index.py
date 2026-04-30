b974b24ffdd69942af2b959c5cf2f7fe1c5307f3
PranavS-23047 <23047@students.mrgs.school.nz>
2026-04-30 13:14:58 +1200

    Making Changes

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
-# the import statement
 import tkinter as tk
 root = tk.Tk()
 
 # setting the title of the program
-root.title("Guess The Number")
 
+root.title('Guess The Number')
 
-label = tk.Label(root, text="Hello")
-label.pack()
+root.geometry("1280x720")
+bg = tk.PhotoImage(file="images/mainbg.png")
+
+canvas1= tk.Canvas(root, width=1280, height=720)
+canvas1.pack(fill="both", expand=True)
+canvas1.create_image(0,0, image=bg, anchor="nw")
+
 root.mainloop()
