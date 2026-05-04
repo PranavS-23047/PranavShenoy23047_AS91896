8369af854454c73ba5b6259e9f7e8c9b326d6077
PranavS-23047 <23047@students.mrgs.school.nz>
2026-05-05 09:38:15 +1200

    Making Changes

--- index.py
+++ index.py
@@ -4,15 +4,16 @@ from tkinter import Frame, Label, BOTH
 
 root = tk.Tk()
 root.title('Guess The Number')
+root.geometry("1915x1000")
 
 background_image = Image.open('images/mainbg.png')
-background_image = background_image.resize((1920, 1080), Image.LANCZOS)
+background_image = background_image.resize((1920, 1080), Image.Resampling.LANCZOS)
 background_image_tk = ImageTk.PhotoImage(background_image)
 
-hangman_frame = Frame(root)
-hangman_frame.pack(fill=BOTH, expand=True)
+gtn_frame = Frame(root)
+gtn_frame.pack(fill=BOTH, expand=True)
 
-image_label = Label(hangman_frame, image=background_image_tk)
+image_label = Label(gtn_frame, image=background_image_tk)
 image_label.image = background_image_tk
 image_label.place(x=0, y=0, relwidth=1, relheight=1)
 
