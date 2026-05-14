5280e2647cb1ef569cf131eb6f6098ab30398da4
PranavS-23047 <23047@students.mrgs.school.nz>
2026-05-14 13:29:17 +1200

    happening

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
-from tkinter import *
-import tkinter as tk
+from tkinter import Tk, Canvas, Button, Frame
 from PIL import ImageTk, Image
-from tkinter import Frame
-from index import gtn_frame
 
 win = Tk()
 win.title("Guess the Number")
-
 win.geometry("1915x1000")
 
 original_image = Image.open("main_bg.png")
 
 canvas = Canvas(win)
-canvas.pack(fill=BOTH, expand=True)
-
+canvas.pack(fill="both", expand=True)
 
 def resize_image(event):
     new_width = event.width
@@ -25,11 +20,19 @@ def resize_image(event):
 
     canvas.delete("all")
     canvas.create_image(0, 0, image=photo, anchor='nw')
-
     canvas.image = photo
 
+canvas.bind("<Configure>", resize_image)
 resize_image(type('obj', (object,), {'width': 700, 'height': 450}))
 
-canvas.bind("<Configure>", resize_image)
+gtn_frame = Frame(win)
+gtn_frame.pack()
+
+button_canvas = Canvas(gtn_frame, width=400, height=60)
+button_canvas.pack()
+
+button1 = Button(button_canvas, text="CLICK HERE TO BEGIN", borderwidth=0, highlightthickness=0,
+                  bg="#feefc8", font=("Bobby Jones Soft", 26))
+button_canvas.create_window(0, 0, anchor="nw", window=button1)
 
 win.mainloop()
