0d17236fc1125fef3e509e25a2b16afd91249492
23047 <23047@students.mrgs.school.nz>
2026-06-16 09:35:49 +1200

    Wrapped up the homepage, need to start on the instruction page to follow

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ root = tk.Tk()
 root.title("Guess the Number")
 root.geometry("1915x1000")
 
-original_image = Image.open("main_bg.png")
+bg_image = Image.open("main_bg.png")
 
 canvas = tk.Canvas(root)
 canvas.pack(fill="both", expand=True)
@@ -15,7 +15,7 @@ def resize_image(event):
     new_width = event.width
     new_height = event.height
 
-    resized_image = original_image.resize((new_width, new_height), Image.LANCZOS)
+    resized_image = bg_image.resize((new_width, new_height), Image.LANCZOS)
 
     photo = ImageTk.PhotoImage(resized_image)
 
@@ -33,11 +33,18 @@ button1_font = font.Font(family="Bobby Jones Soft")
 button1 = tk.Button(
     root,
     text="CLICK HERE TO BEGIN",
-    font=button1_font,
+    font=(button1_font, 14),
+    padx=0,
+    pady=0,
+    bd=0,
     bg="#feefc8",
+    anchor='center',
+    activebackground="#c7b897",
+    height=5,
+    width=15,
     )
 button1.pack(fill=tk.BOTH, expand=True)
-button1.place(x=850, y=100)
+button1.place(x=810, y=106.5)
 button1.config(height=5, width=25)
 
 root.mainloop()
