3f93088c6699492ecff8091819e0c3149f2daf1b
23047 <23047@students.mrgs.school.nz>
2026-06-23 09:51:40 +1200

    Wrapped up the homepage, need to start on the instruction page to follow

--- index.py
+++ index.py
@@ -1,40 +1,40 @@
+# module imports
 import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import font
 
-
+# window setup
 root = tk.Tk()
 root.title("Guess the Number")
 root.geometry("1915x1000")
 
+# canvas and image backgrounds
 bg_image = Image.open("main_bg.png")
-
 canvas = tk.Canvas(root)
 canvas.pack(fill="both", expand=True)
 
+# image resizing
 def resize_image(event):
     new_width = event.width
     new_height = event.height
-
     resized_image = bg_image.resize((new_width, new_height), Image.LANCZOS)
-
     photo = ImageTk.PhotoImage(resized_image)
-
     canvas.delete("all")
     canvas.create_image(0, 0, image=photo, anchor='nw')
     canvas.image = photo
 
+# frames and canvas
 canvas.bind("<Configure>", resize_image)
 resize_image(type('obj', (object,), {'width': 700, 'height': 450}))
-
 gtn_frame = tk.Frame(root)
 gtn_frame.pack()
 
-button1_font = font.Font(family="Bobby Jones Soft")
+# button design and setup
+button1_font = font.Font(family="Bobby Jones Soft", size=25)
 button1 = tk.Button(
     root,
     text="CLICK HERE TO BEGIN",
-    font=(button1_font, 25),
+    font=button1_font,
     bd=0,
     bg="#feefc8",
     anchor='center',
@@ -42,7 +42,14 @@ button1 = tk.Button(
     height=1,
     width=20,
     )
-button1.pack(fill=tk.BOTH, expand=True)
-button1.place(x=775, y=106)
+button1.place(x=762, y=135)
+
+def on_enter(event):
+    button1.config(bg="#c7b897", fg="black")
+def on_leave(event):
+    button1.config(bg="#feefc8", fg="black")
+
+button1.bind("<Enter>", on_enter)
+button1.bind("<Leave>", on_leave)
 
 root.mainloop()
